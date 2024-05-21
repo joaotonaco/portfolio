e54ac708312d77079a70124695ed941ed89c5794
João Gabriel <joao.tonaco@proton.me>
2024-05-21 15:54:37 -0300

    fix svg component

--- src/components/squarecloud.tsx
+++ src/components/squarecloud.tsx
@@ -14,14 +14,14 @@ export function SquareCloud({ className }: SquareCloudProps) {
 		>
 			<title>Square Cloud Logo</title>
 			<path
-				fill-rule="evenodd"
-				clip-rule="evenodd"
+				fillRule="evenodd"
+				clipRule="evenodd"
 				d="M97 23H9V111H97V23ZM3 17V117H103V17H3Z"
 				fill="white"
 			/>
 			<path
-				fill-rule="evenodd"
-				clip-rule="evenodd"
+				fillRule="evenodd"
+				clipRule="evenodd"
 				d="M111 9H23V23H17V3H117V103H97V97H111V9ZM91 103V97H23V29H17V103H91Z"
 				fill="white"
 			/>
