0007bbba9502b7c151be223360fca4c810e1f9b6
João Gabriel <joao.tonaco@proton.me>
2024-05-21 10:17:52 -0300

    implement biomejs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,18 +5,18 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "João Gabriel Tonaco",
-  description: "Front-end developer",
+	title: "João Gabriel Tonaco",
+	description: "Front-end developer",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 }
