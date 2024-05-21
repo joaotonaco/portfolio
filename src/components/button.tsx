fd6bb5ace17a5d6c8d02a3d04cfafca38db5ecd6
João Gabriel <joao.tonaco@proton.me>
2024-05-21 16:16:25 -0300

    fix linter configuration

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,5 +4,13 @@ import type React from "react";
 type ButtonProps = React.HTMLAttributes<HTMLButtonElement>;
 
 export function Button({ className, ...props }: ButtonProps) {
-	return <button className={cn("bg-primary text-background px-8 py-2 rounded-full font-semibold hover:bg-primary/80", className)} {...props} />;
+	return (
+		<button
+			className={cn(
+				"bg-primary text-background px-8 py-2 rounded-full font-semibold hover:bg-primary/80",
+				className,
+			)}
+			{...props}
+		/>
+	);
 }
