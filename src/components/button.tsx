4da3227a8f36a6683ce5ddfa99d7957eb1e35b5f
João Gabriel <joao.tonaco@proton.me>
2024-05-21 11:14:11 -0300

    style button component

---
+++ src/components/button.tsx
@@ -0,0 +1,8 @@
+import { cn } from "@/lib/utils";
+import type React from "react";
+
+type ButtonProps = React.HTMLAttributes<HTMLButtonElement>;
+
+export function Button({ className, ...props }: ButtonProps) {
+	return <button className={cn("bg-primary text-background px-8 py-2 rounded-full font-semibold hover:bg-primary/80", className)} {...props} />;
+}
